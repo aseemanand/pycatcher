3fb3e82df907669eb4866d2af47dfef86c7a0851
sarikajagadish <149593180+sarikajagadish@users.noreply.github.com>
2024-09-28 15:50:13 -0500

    Added a couple more functions

--- src/anomaly_detection.py
+++ src/anomaly_detection.py
@@ -1,7 +1,10 @@
+import math
 import numpy as np
 import pandas as pd
-from sklearn.base import BaseEstimator
+from typing import Union
 from pyod.models.mad import MAD
+from sklearn.base import BaseEstimator
+from statsmodels.tsa.stattools import acf
 
 
 def find_outliers_iqr(df: pd.DataFrame) -> pd.DataFrame:
@@ -94,4 +97,72 @@ def sum_of_squares(array: np.ndarray) -> float:
     sum_of_squares_value = np.sum(flattened_array ** 2)
 
     return sum_of_squares_value
+
+
+def get_ssacf(residuals: np.ndarray, df_pandas: pd.DataFrame) -> float:
+    """
+    Get the sum of squares of the autocorrelation function (ACF) of the residuals.
+
+    Args:
+        residuals (np.ndarray): A NumPy array containing the residuals.
+        df_pandas (pd.DataFrame): A pandas DataFrame containing the data.
+
+    Returns:
+        float: The sum of squares of the ACF of the residuals.
+    """
+
+    # Calculate the number of lags based on the square root of the data length
+    range_var = len(df_pandas.index)
+    nlags = math.isqrt(range_var) + 45
+
+    # Compute the ACF of the residuals
+    acf_array = acf(residuals, nlags=nlags, fft=True)
+
+    # Calculate the sum of squares of the ACF values
+    ssacf = sum_of_squares(acf_array)
+
+    return ssacf
+
+
+def get_outliers_today(model_type):
+
+    df = anomaly_mad(model_type)
+    df = df.tail(1)
+
+    latest = df.index[-1].date().strftime('%Y-%m-%d')
+    p = pd.Timestamp.now().to_period('D')
+    current_date = p.to_timestamp().strftime('%Y-%m-%d')
+
+    if latest == current_date:
+        return df
+
+    return "No Outliers Today!"
+
+
+def get_outliers_today(model_type: BaseEstimator) -> Union[pd.DataFrame, str]:
+    """
+    Get the outliers detected today using the anomaly_mad method.
+
+    Args:
+        model_type (BaseEstimator): A fitted model object used to detect outliers.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing today's outliers if detected.
+        str: A message indicating no outliers were found today.
+    """
+
+    # Get the DataFrame of outliers from anomaly_mad and select the latest row
+    df = anomaly_mad(model_type).tail(1)
+
+    # Extract the latest outlier's date
+    latest_outlier_date = df.index[-1].date().strftime('%Y-%m-%d')
+
+    # Get the current date
+    current_date = pd.Timestamp.now().strftime('%Y-%m-%d')
+
+    # Check if the latest outlier occurred today
+    if latest_outlier_date == current_date:
+        return df
+
+    return "No Outliers Today!"
 
